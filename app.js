dcc871d9715c353cca7d5b5b368189c7293241e2
Joseph Price <josephprice747@gmail.com>
2023-01-19 15:52:02 +0000

    updated stuff

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
-let myName = prompt ("Who dares enter my domain?");
+// function to get the user's name
+function getName() {
+  let myName = prompt("Who dares enter my domain?");
 
-console.log (myName);
+  document.write(myName);
+}
+
+// function to get the age of the user, and responding based on this
+function getAge() {
+  let age = prompt("For how long have you lived, mortal?");
+
+  let message;
+
+  if (age <= 3) {
+    message = "Your skills are impressive, for a baby";
+  } else if (age <= 11) {
+    message = "The young shall be spared my long and boring backstory";
+  } else if (age <= 17) {
+    message =
+      "Eat your five fruits and vegetables, you cant live off cake alone";
+  } else if (age <= 29) {
+    message =
+      "You are the prime age for cake testing, get in touch for a free sample";
+  } else {
+    message = "LAAAAAMME. Get outta here ! ! !";
+  }
+
+  document.write(message);
+}
+
+// function to get the time from the user
+function getTime() {
+  let time = prompt("What time is it (0-23)");
+
+  let message;
+
+  if (time <= 11) {
+    message = "Good morning";
+  } else if (time <= 16) {
+    message = "Good afternoon";
+  } else if (time <= 23) {
+    message = "Good evening";
+  } else {
+    message = "Wait, do you live on saturn or something?";
+  }
+
+  document.write(message);
+}
